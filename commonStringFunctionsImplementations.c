b5743780e6e17f84cd23e3b4b6a1080c20072424
Gaston Seneza <gseneza@gmail.com>
2017-01-08 16:37:28 -0600

    Add strcmp() implementation

--- commonStringFunctionsImplementations.c
+++ commonStringFunctionsImplementations.c
@@ -72,5 +72,43 @@ char* my_strstr(const char* haystack, const char* needle) {
 }
 
 
+//implementing strcmp() from scratch
+//Compares two strings' length
+int my_strcmp(const char* str1, const char* str2) {
+    if (str1 == NULL && str2 == NULL) {
+        return 0;
+    }
+    if (str1 == NULL) {
+        return 0 - *str2;
+    }
+    if (str2 == NULL ) {
+        return *str1;
+    }
+    
+    const char* ch1 = str1;
+    const char* ch2 = str2;
+    
+    while(*ch1 != '\0' && *ch2 != '\0') {
+        if(*ch1 != *ch2) {
+            return *ch1 - *ch2;
+        }
+        ch1 ++;
+        ch2++;
+    }
+    if (*ch1 == '\0' && *ch2 != '\0') {
+        return 0 - *ch2;
+    }
+    if (*ch1 != '\0' && *ch2 == '\0') {
+        return *ch1;
+    }
+    return 0;
+}
+
+
+
+
+
+
+
 
 
